602ffb4bb8d5151f55cc30e9bc185ff45417502e
RomannaBidnyk <romanna.bidnyk@gmail.com>
2025-03-25 14:59:55 -0400

    tasks 3-6

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -27,3 +27,55 @@ def read_employees():
 
 employees = read_employees()
 print(f"Employees: {employees}")
+
+
+# Task 3
+print("\nTask 3")
+
+
+def column_index(column):
+    return employees["fields"].index(column)
+
+
+employee_id_column = column_index("employee_id")
+print(f"Index for employee_id is {employee_id_column}")
+
+# Task 4
+print("\nTask 4")
+
+
+def first_name(row_number):
+    first_name_col = column_index("first_name")
+    return employees["rows"][row_number][first_name_col]
+
+
+print(first_name(0))
+
+# Task 5
+print("\nTask 5")
+
+
+def employee_find(employee_id):
+    def employee_match(row):
+        return int(row[employee_id_column]) == employee_id
+
+    matches = list(filter(employee_match, employees["rows"]))
+    return matches
+
+
+print(employee_find(15))
+
+# Task 6
+print("\nTask 6")
+
+
+def employee_find_2(employee_id):
+    matches = list(
+        filter(
+            lambda row: int(row[employee_id_column]) == employee_id, employees["rows"]
+        )
+    )
+    return matches
+
+
+print(employee_find(15))
